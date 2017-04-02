49dcbac23faac4d3cb2148527c4651b32919f7a4
Florian <f.maul@fme.de>
2017-04-02 17:23:57 +0200

    debugging

--- assets/js/main.js
+++ assets/js/main.js
@@ -481,12 +481,12 @@ var main = (function($) { var _ = {
 	init: function() {
 
 		window.screen.orientation.onchange = function() {
-						  if (this.type.startsWith('landscape')) {
+			if (this.type.startsWith('landscape')) {
 				document.querySelector('body').webkitRequestFullscreen();
-			  } else {
+		  } else {
 				document.webkitExitFullscreen();
-			  }
-			};
+		  }
+		};
 
 		// IE<10: Zero out transition delays.
 			if (skel.vars.IEVersion < 10) {
